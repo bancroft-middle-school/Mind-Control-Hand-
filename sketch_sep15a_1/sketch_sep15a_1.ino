ab883ca7a9d53e4fbdd0f34358bac74cce368e89
kevin578 <kbriggs@aapl000894.local>
2018-03-08 21:58:03 -0500

    updated with code for for temp sensor and lcd

--- sketch_sep15a_1/sketch_sep15a_1.ino
+++ sketch_sep15a_1/sketch_sep15a_1.ino
@@ -10,13 +10,19 @@ void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("hello, world!");
+  lcd.print("Current Temp:");
 }
 
 void loop() {
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
+  
+  int rawVoltage = analogRead(A0); //reads raw voltage from the LM35 output
+  int celciusTemp = rawVoltage * 0.48828125;
+  celciusTemp = celciusTemp * 1.8 + 32;//converts that into celcus
+  Serial.println(celciusTemp); //prints celcius to Serial monitor
+  delay(500);
   // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+  lcd.print(celciusTemp);
 }
